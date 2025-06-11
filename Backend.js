73ed8e199ffd6c41fb02b9825946beb4ae82c99d
rretan <196845036+rretan@users.noreply.github.com>
2025-06-11 15:07:04 -0400

    Working access to node server. Basic access.

--- Backend.js
+++ Backend.js
@@ -3,26 +3,40 @@ const { stat } = require('fs');
 const app = express();
 const port = 8081
 
-
+const cors = require('cors');
+app.use(cors()); // Using the CORS middleware
 var dbCon = require('./connectToDB.js').dbCon; // connect to DB
 
 app.use(express.static('public'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
-app.get ('/info/:dynamic', (req, res) => {
+app.get ('/{*any}', (req, res) => {
       const { dynamic } = req.params
       const { key }= req.query
       console.log (dynamic,key)
       res.status(200).json({ info: 'preset text' });
 })
 
-
-app.post('/', (req, res) => {
-      const {parcel} = req.body
-      console.log(parcel)
-      if (!parcel) {
-           return res.status(400).send({status: 'failed'})
-      }
-      res.status(200).send({status: 'recived'})
+app.post('/{*any}', (req, res) => {
+    try {
+        console.log("WORK")
+        setHeader(req, res);
+        console.log(req.body.username)
+        if (req.body.username=="test") {
+            res.send("TEST WORKED");
+        } else {
+            res.send("Post request not accounted for");
+        }
+        
+    } catch (error) {
+      	res.send(error);
+	}
 })
+
 app.listen(port, () => console.log(`we have a connection on port: ${port}`));
+
+function setHeader (request, response) {
+	response.setHeader('Access-Control-Allow-Credentials', 'true');
+    	if (request.headers.origin) response.setHeader('Access-Control-Allow-Origin', request.headers.origin);
+}
